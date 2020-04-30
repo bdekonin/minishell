ad9c6c6e45b373fc58cf7f0ce1d2052382505da2
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-30 20:46:33 +0200

    added cd version 2.0

--- srcs/cd.c
+++ srcs/cd.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/28 22:58:42 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/30 19:01:15 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/30 20:41:52 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,42 +21,36 @@ static void		updatepath(t_vars *v)
 // Version 2.0
 int				cd(t_vars *v, char **params)
 {
-	if (ft_wordcount(v->line) > 2 && \
-	params[1][ft_strlen(params[1]) - 1] == 92)
-		removespace(v, params + 1);
-	ft_printf("line = %s\n", v->test);
-	for (int i = 0; i < ft_wordcount(v->line); i++)
-	{
-		ft_printf("str1 = [%s]\t[%s]\n", params[i], v->argv[i]);
-	}
+	// if (ft_wordcount(v->line) > 2 && \
+	// params[1][ft_strlen(params[1]) - 1] == 92)
+	// 	removespace(v, params + 1);
 	if (!v->argv[1])
-		chdir("/Users/bdekonin");
-	else if (chdir(v->argv[1]) == -1)
+	{
+		if (chdir("/Users/bdekonin") < 0)
+			perror("ERROR\n");
+	}
+	else if (chdir(params[1]) == -1)
 		perror("ERROR\n");
+	// for (int i = 0; i < ft_wordcount(v->line); i++)
+	// 	ft_printf("argv[%d] = [%s]\t[%s]\n", i, params[i], v->argv[i]);
 	updatepath(v);
+	if (params[2] && !ft_strncmp(params[2], ";", 3))
+		cmd(v, params + 3);
+	else if (params[3] && !ft_strncmp(params[3], ";", 3))
+		cmd(v, params + 4);
 	return (1);
 }
+// cd srcs ; cd ..
+// argv[0] = [cd]   [cd]
+// argv[1] = [srcs] [srcs]
+// argv[2] = [;]    [;]
+// argv[3] = [cd]   [cd]
+// argv[4] = [..]   [..]
 
-// REDO VERSION 1.0
-// int				cd(t_vars *v, char **params)
-// {
-// 	v->ptr = NULL;
-// 	if (ft_wordcount(v->line) > 2 && \
-// 	params[1][ft_strlen(params[1]) - 1] == 92)
-// 		removespace(v, params + 1);
-// 	if (v->ptr && ft_wordcount(v->line) > 2)
-// 	{
-// 		movestring(v);
-// 		if (v->ptr && chdir(v->ptr) == -1)
-// 			perror("ERROR\n");
-// 	}
-// 	else
-// 	{
-// 		if (chdir(params[1]) == -1)
-// 			perror("ERROR");
-// 	}
-// 	updatepath(v);
-// 	if (params[2] && *(params[2]) == ';')
-// 		cmd(v, params + 3);
-// 	return (1);
-// }
+// cd Test\ ing/ ; cd ..
+// argv[0] = [cd]          [cd]
+// argv[1] = [Test\ ing]   [Test\ ing]
+// argv[2] = [^^^^]        [^^^^]
+// argv[3] = [0x]          [0x]
+// argv[4] = [cd]          [cd]
+// argv[5] = [..]          [..]
