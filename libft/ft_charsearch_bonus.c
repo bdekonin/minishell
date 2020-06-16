96dcb3635288520529b6b1c1d4bdc5fd519c3a38
Bob de konink <bdekonin@student.codam.nl>
2020-06-16 12:03:41 +0200

    added a bool version

--- libft/ft_charsearch_bonus.c
+++ libft/ft_charsearch_bonus.c
@@ -6,21 +6,37 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/16 23:43:02 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/05/16 23:44:39 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/06/16 11:59:49 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_charsearch(char *str, int c)
+int	ft_charsearch_bool(char *str, char c)
 {
 	int i;
-
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] == c)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_charsearch(char *str, char c)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
 			return (i);
+		}
 		i++;
 	}
 	return (0);
