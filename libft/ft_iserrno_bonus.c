7c8614c46469a978daeebfc9404794e507c8fc72
bdekonin <bdekonin@student.codam.nl>
2020-07-09 16:17:55 +0200

    libft update

---
+++ libft/ft_iserrno_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_iserrno_bonus.c                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/09 16:12:20 by bdekonin      #+#    #+#                 */
+/*   Updated: 2020/07/09 16:13:59 by bdekonin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <errno.h>
+
+int ft_iserrno(int error)
+{
+	if (errno == error)
+		return (1);
+	else
+		return (0);
+}
