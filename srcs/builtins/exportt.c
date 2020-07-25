26bb61c27f49552ed0c24717bf76d794a43ab207
Lars Verdoes <lars@air.home>
2020-07-25 12:44:36 +0200

    export++

--- srcs/builtins/exportt.c
+++ srcs/builtins/exportt.c
@@ -6,11 +6,60 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/29 16:14:06 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/07/24 13:29:52 by lverdoes      ########   odam.nl         */
+/*   Updated: 2020/07/25 12:33:50 by lverdoes      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../main.h"
+
+
+static char		**free_array(char **array, size_t i)
+{
+	while (i > 0)
+	{
+		i--;
+		free(array[i]);
+	}
+	free(array);
+	return (NULL);
+}
+
+static size_t	get_size_of_src(char **src)
+{
+	size_t i;
+
+	if (!src)
+		return (0);
+	i = 0;
+	while (src[i] != NULL)
+		i++;
+	return (i);
+}
+
+char			**ft_realloc(char **src, char *line)
+{
+	size_t	size;
+	char	**dst;
+	size_t	i;
+
+	size = get_size_of_src(src);
+	dst = ft_calloc(size + 2, sizeof(char *));
+	if (!dst)
+		return (free_array(src, size));
+	i = 0;
+	while (i < size)
+	{
+		dst[i] = ft_strdup(src[i]);
+		i++;
+	}
+	free_array(src, size);
+	dst[i] = ft_strdup(line);
+	if (!dst[i])
+		return (free_array(dst, i));
+	src = dst;
+	return (dst);
+}
+
 
 
 /*
@@ -33,69 +82,38 @@
 **
 */
 
-// static int	find_env_var_name(t_env **head, char *name, char *content)
-// {
-// 	t_env	*tmp;
-	
-// 	tmp = *head;
-// 	while (tmp)
-// 	{
-// 		if (!ft_strncmp(tmp->name, name, ft_strlen(name)))
-// 		{
-// 			free(name);
-// 			free(tmp->content);
-// 			tmp->content = content;
-// 			return (1); //existing name found in env list
-// 		}
-// 		tmp = tmp->next;
-// 	}
-// 	return (0); //new name found in env list
-// }
-
-static int	do_dollar(t_vars *v, char *dst, char *src, size_t *i, size_t *j)
+static int	find_env_var_name(t_env **head, char *name, char *content)
 {
-	size_t env_len;
-	char *env_name;
-	char *env_content;
-
-	*i += 1;
-	env_len = 0;
-	while (src[*i + env_len] != '\0' && src[*i + env_len] != '\"' && src[*i + env_len] != ' ' && src[*i + env_len] != '\\' && src[*i + env_len] != '$' && src[*i + env_len] != '/')
-		env_len++;
-	if (env_len == 0)
-		return (0); //env var name has len of 0. Don't delete this check.
-	env_name = src + *i;
-	env_content = find_environment_variable(v, env_name, &env_len);
-	if (!env_content)
-		return (0); //env name not in env list
-	// maybe expand dst here?
-	ft_strlcat(dst + *j, env_content, ft_strlen(dst) + 1);
-	*i += env_len - 1;
-	*j += ft_strlen(env_content);
-	return (1);
+	t_env	*tmp;
+	
+	tmp = *head;
+	while (tmp)
+	{
+		if (!ft_strncmp(tmp->name, name, ft_strlen(name)))
+		{
+			free(name);
+			free(tmp->content);
+			tmp->content = content;
+			return (1); //existing name found in env list
+		}
+		tmp = tmp->next;
+	}
+	return (0); //new name found in env list
 }
 
-static int	do_double_quote(t_vars *v, char *dst, char *src, size_t *i, size_t *j)
+static int	copy_double_quote(char *dst, char *src, size_t *i, size_t *j)
 {
 	*i += 1;
 	while (src[*i] != '\0' && src[*i] != '\"')
 	{
-		if (src[*i] == '$')
-		{
-			do_dollar(v, dst, src, i, j); //check return
-			*i += 1;
-		}
-		else
-		{	
-			dst[*j] = src[*i];
-			*i += 1;
-			*j += 1;
-		}
+		dst[*j] = src[*i];
+		*i += 1;
+		*j += 1;
 	}
 	return (1);
 }
 
-static int	do_single_quote(char *dst, char *src, size_t *i, size_t *j)
+static int	copy_single_quote(char *dst, char *src, size_t *i, size_t *j)
 {
 	*i += 1;
 	while (src[*i] != '\0' && src[*i] != '\'')
@@ -107,15 +125,7 @@ static int	do_single_quote(char *dst, char *src, size_t *i, size_t *j)
 	return (1);
 }
 
-static int	do_hashtag(char *src, size_t *i)
-{
-	*i += 1;
-	while (src[*i] != '\0')
-		*i += 1;
-	return (1);
-}
-
-static int	do_backslash(char *dst, char *src, size_t *i, size_t *j)
+static int	copy_backslash(char *dst, char *src, size_t *i, size_t *j)
 {
 	*i += 1;
 	dst[*j] = src[*i];
@@ -123,34 +133,28 @@ static int	do_backslash(char *dst, char *src, size_t *i, size_t *j)
 	return (1);
 }
 
-static int parse_input(t_vars *v, char *dst, char *src)
+static int parse_input(char *dst, char *src)
 {
-	int ret;
 	size_t i; //index in src
 	size_t j; //index in dst
 
-	ret = 1;
 	i = 0;
 	j = 0;
 	while (src[i] != '\0')
 	{
 		if (src[i] == '\\')
-			ret = do_backslash(dst, src, &i, &j);
-		else if (src[i] == '#' && i == 0)
-			ret = do_hashtag(src, &i);
+			copy_backslash(dst, src, &i, &j);
 		else if (src[i] == '\'')
-			ret = do_single_quote(dst, src, &i, &j);
+			copy_single_quote(dst, src, &i, &j);
 		else if (src[i] == '\"')
-			ret = do_double_quote(v, dst, src, &i, &j);
-		else if (src[i] == '$')
-			ret = do_dollar(v, dst, src, &i, &j);
+			copy_double_quote(dst, src, &i, &j);
+		else if (src[i] == '#' && i == 0)
+			return (1);
 		else
 		{
 			dst[j] = src[i];
 			j++;
 		}
-		if (ret != 1)
-			return (ret);
 		i++;
 	}
 	return (1);
@@ -168,17 +172,47 @@ static size_t get_size_params(char **params)
 	size_t size;
 
 	size = 0;
-	while (params[size])
+	while (params[size] != NULL)
 		size++;
 	return (size);
 }
 
-int 			exportt(t_vars *v, t_cmd *cmd, char **params) //tttttt?
+static int	add_new_env_vars(t_vars *v, char **dst)
+{
+	size_t i;
+	char *ptr;
+	char *name;
+	char *cont;
+
+	i = 1; // 0 = export
+	while (dst[i] != NULL)
+	{
+		ptr = ft_strchr(dst[i], '=');
+		*ptr = '\0';
+		name = dst[i];
+		if (!ptr)
+			return (0); //malloc
+		cont = ft_strdup(ptr + 1);
+		if (!cont)
+		{
+			free (name);
+			return (0); //malloc
+		}
+		if (!find_env_var_name(&v->env_head, name, cont))
+		{
+			env__ft_lstadd_back(&v->env_head, env__ft_lstnew(name, cont));
+			env__ft_lstmove_back("_", v->env_head);	
+		}
+		i++;
+	}
+	return (1);
+}
+
+int 			exportt(t_vars *v, t_cmd *cmd, char **params)
 {
 	size_t	i;
 	size_t	d;
 	size_t	size;
-	int 	ret;
 	char	**dst;
 	
 	size = get_size_params(params);
@@ -187,34 +221,21 @@ int 			exportt(t_vars *v, t_cmd *cmd, char **params) //tttttt?
 		printf("alleen export dus\n");
 		return (0); //return (export_declare_-x_list);
 	}
-	dst = ft_calloc(size + 1, sizeof(char *));
-	if (!dst)
-		return (0); //malloc
 	i = 0;
+	dst = 0;
 	d = check_declare_option(params[0]);
 	while (i + d < size)
 	{
-		dst[i] = ft_calloc(ft_strlen(params[i + d] + 1), sizeof(char));
-		if (!dst[i])
-		{
-			ft_free_array((void **)dst, i);
-			return (0); //malloc
-		}
-		ret = parse_input(v, dst[i], params[i + d]);
-		if (ret == 0)
-			return (0); //some error happened, like malloc
+		dst = ft_realloc(dst, params[i + d]);
+		if (!dst)
+			return (0);
+		parse_input(dst[i], params[i + d]);
 		i++;
 	}
+	add_new_env_vars(v, dst);
+	ft_free_array((void **)dst, i);
 	if (!sethistory(&v->history_head, v->line, "0"))
 		return (0);
 	(void)(cmd);
 	return (1);
 }
-
-
-
-		// if (!find_env_var_name(&v->env_head, array[i], array[i + 1]))
-		// {
-		// 	env__ft_lstadd_back(&v->env_head, env__ft_lstnew(array[i], array[i + 1]));
-		// 	env__ft_lstmove_back("_", v->env_head);	
-		// }
