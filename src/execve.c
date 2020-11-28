39a3af28aed2307137be81bfb9aa456023566321
bdekonin <bdekonin@student.codam.nl>
2020-11-28 16:09:16 +0100

    norm

--- src/execve.c
+++ src/execve.c
@@ -6,7 +6,7 @@
 /*   By: lverdoes <lverdoes@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/13 19:05:00 by lverdoes      #+#    #+#                 */
-/*   Updated: 2020/11/28 15:59:34 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/11/28 16:08:54 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,31 +60,16 @@ static int	run_execve(t_vars *v, char *path, char **params, char **envp)
 	}
 	else
 		waitpid(-1, &stat, 0);
-	free(path);
-	ft_free_array((void **)envp, env__ft_lstsize(v->env));
 	return (WEXITSTATUS(stat));
 }
 
-static int	findtype_execve(t_vars *v, char **params, char **path)
+static void	freething(t_vars *v, char **envp, char *path)
 {
-	int ret;
-
-	ret = 0;
-	if (!ft_strncmp(params[0], "./", 2))
+	if (!envp)
 	{
-		ret = handle_relative(v, path, params[0]);
-		if (ret == FILEPERMISSIONS || ret == FILENOTFOUND)
-			return (ret);
+		free(path);
+		ft_exit_error(v, EXIT_FAILURE, 1);
 	}
-	else
-	{
-		ret = handle_static(v, path, params[0]);
-		if (ret == FILEPERMISSIONS)
-			return (ret);
-		else if (ret == FILENOTFOUND)
-			return (FILENOTFOUND * 10);
-	}
-	return (FILEFOUND);
 }
 
 int			ft_execve(t_vars *v, char **params)
@@ -94,15 +79,24 @@ int			ft_execve(t_vars *v, char **params)
 	int		ret;
 
 	path = NULL;
-	ret = findtype_execve(v, params, &path);
-	if (ret == FILEERROR)
-		ft_exit_error(v, EXIT_FAILURE, 1);
-	envp = env_list_to_array(v);
-	if (!envp)
+	if (!ft_strncmp(params[0], "./", 2))
 	{
-		free(path);
-		ft_exit_error(v, EXIT_FAILURE, 1);
+		ret = handle_relative(v, &path, params[0]);
+		if (ret == FILEPERMISSIONS || ret == FILENOTFOUND)
+			return (ret);
 	}
+	else
+	{
+		ret = handle_static(v, &path, params[0]);
+		if (ret == FILEPERMISSIONS)
+			return (ret);
+		else if (ret == FILENOTFOUND)
+			return (FILENOTFOUND * 10);
+	}
+	envp = env_list_to_array(v);
+	freething(v, envp, path);
 	ret = run_execve(v, path, params, envp);
+	free(path);
+	ft_free_array((void **)envp, env__ft_lstsize(v->env));
 	return (ret);
 }
